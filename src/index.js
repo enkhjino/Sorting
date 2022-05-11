eb0b5e89e4bd5d7608400b2614ebbe0eabf3e392
enkhjino <96076106+enkhjino@users.noreply.github.com>
2022-05-11 13:55:01 -0700

    mergeRecursion

--- src/index.js
+++ src/index.js
@@ -12,3 +12,12 @@ function mergeSort(arr1, arr2) {
   }
   return result;
 }
+
+function mergeRecursion(arr) {
+  if (arr.length <= 1) return arr;
+  var firstHalf = mergeRecursion(arr.slice(0, arr.length / 2));
+  var secondHalf = mergeRecursion(arr.slice(arr.length / 2));
+  return mergeSort(firstHalf, secondHalf);
+}
+
+//console.log(mergeRecursion([4,2,3,5]))
