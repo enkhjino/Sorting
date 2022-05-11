3869198ce36e3741b87f4b12dbab80b22f9f223b
enkhjino <96076106+enkhjino@users.noreply.github.com>
2022-05-11 13:54:18 -0700

    merge sort

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//merge sort
+function mergeSort(arr1, arr2) {
+  var result = [...arr1, ...arr2];
+  for (var i = 0; i < result.length; i++) {
+    for (var j = i + 1; j < result.length; j++) {
+      if (result[i] > result[j]) {
+        var temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+      }
+    }
+  }
+  return result;
+}
